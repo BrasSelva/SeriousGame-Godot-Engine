6c9650a8f1b7d4b211a106ea37586734a01431d9
brass94 <brassica.selvaratnam@gmail.com>
2026-04-28 16:48:48 +0200

    implémentation du menu principal et de la navigation vers la sélection

--- Scripts/Autoloads/GameManager.gd
+++ Scripts/Autoloads/GameManager.gd
@@ -3,20 +3,20 @@ extends Node
 var human_score: int = 50
 var ai_score: int = 50
 var quality_score: int = 50
-var time_left: int = 4 # Youcef commence avec 4 heures
+var time_left: int = 4 
+
+var unlocked_skills: Array = []
 
 func update_youcef_stats(h_delta, ai_delta, q_delta, t_delta):
 	human_score = clamp(human_score + h_delta, 0, 100)
 	ai_score = clamp(ai_score + ai_delta, 0, 100)
 	quality_score = clamp(quality_score + q_delta, 0, 100)
 	time_left -= t_delta
-	
-	print("MAJ Stats - Humain:", human_score, " AI:", ai_score, " Qualité:", quality_score, " Temps restant:", time_left)
+	print("Stats: H:", human_score, " IA:", ai_score, " Q:", quality_score, " T:", time_left)
 
-var unlocked_skills: Array = [] # Liste des compétences obtenues
+func has_skill(skill_name: String) -> bool:
+	return unlocked_skills.has(skill_name)
 
-# Fonction pour ajouter une compétence
 func unlock_skill(skill_name: String):
-	if not unlocked_skills.has(skill_name):
+	if not has_skill(skill_name):
 		unlocked_skills.append(skill_name)
-		print("🎯 Compétence débloquée : ", skill_name)
